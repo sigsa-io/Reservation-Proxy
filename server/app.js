6842a2062c2a23428ec9db95c86f0e5d5022a121
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-15 23:34:42 -0700

    code clean up and install morgan

--- server/app.js
+++ server/app.js
@@ -1,49 +1,47 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 const httpProxy = require('http-proxy');
+const morgan = require('morgan');
+
+const app = express();
 const apiProxy = httpProxy.createProxyServer();
-const menus = 'http://localhost:3002',
-    reservations = 'http://localhost:3003',
-    reviews = 'http://localhost:3004';
 
+const menus = 'http://localhost:3002';
+const reservations = 'http://localhost:3003';
+const reviews = 'http://localhost:3004';
+
+app.set('port', 3000);
+
+app.use(morgan('dev'));
 app.use('/:restaurantId', express.static(path.resolve('dist')));
-console.log(path.resolve('dist'))
 
-app.all("/menuData/:restaurant_id", function(req, res) {
-  console.log('redirecting to Menu');
+app.all('/menuData/:restaurant_id', (req, res) => {
   apiProxy.web(req, res, {target: menus});
 });
 
-app.all("/bookingCount/:restaurantId", function(req, res) {
-  console.log('redirecting to Reservations');
+app.all('/bookingCount/:restaurantId', (req, res) => {
   apiProxy.web(req, res, {target: reservations});
 });
 
-app.all("/restaurantName/:restaurantId", function(req, res) {
-  console.log('redirecting to Reservations');
+app.all('/restaurantName/:restaurantId', (req, res) => {
   apiProxy.web(req, res, {target: reservations});
 });
 
-app.all("/seatingSize/:restaurantId", function(req, res) {
-  console.log('redirecting to Reservations');
+app.all('/seatingSize/:restaurantId', (req, res) => {
   apiProxy.web(req, res, {target: reservations});
 });
 
-app.all("/targettimeslots/:restaurantId", function(req, res) {
-  console.log('redirecting to Reservations');
+app.all('/targettimeslots/:restaurantId', (req, res) => {
   apiProxy.web(req, res, {target: reservations});
 });
 
-app.all("/reservations/:restaurantId", function(req, res) {
-  console.log('redirecting to Reservations');
+app.all('/reservations/:restaurantId', (req, res) => {
   apiProxy.web(req, res, {target: reservations});
 });
 
-app.all("/restaurants/:restaurant_id/reviews", function(req, res) {
-  console.log('redirecting to Reviews');
+app.all('/restaurants/:restaurant_id/reviews', (req, res) => {
   apiProxy.web(req, res, {target: reviews});
 });
 
-app.listen(3000);
-console.log('listing to port 3000');
+app.listen(app.get('port'));
+console.log('Now listening to ', app.get('port'));
